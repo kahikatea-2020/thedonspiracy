a33794e51e7618e767e0710c2890fc0c352bf52a
Lache Melvin <lache.melvin@gmail.com>
2020-04-17 14:24:21 +1200

    tada

---
+++ client/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+        <div className="rainbowFooter">
+        </div>
+        <div className="footerBar">
+            <img src="./images/footer.png" />
+        </div>
+        </>
+    )
+}
+
+export default Footer
